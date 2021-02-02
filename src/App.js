3d66e6bc494d778cd423e0fea49a44303aea10bb
relaxedcoder <devjit.menghani@gmail.com>
2021-02-02 11:22:53 +0530

    Simplified the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ReactTransliterate } from 'react-transliterate'
+import 'react-transliterate/dist/index.css'
+
+const App = () => {
+    const [text, setText] = useState('');
+
+    useEffect(() => {
+        var textarea = document.getElementsByTagName('textarea')[0]
+        textarea.placeholder = 'Type something...'
+        
+        textarea.focus()
+    }, [])
+
+    return (
+        <div id='container'>
+            <h2>English to Hindi Transliterate</h2>
+            <ReactTransliterate
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                lang='hi'
+                Component='textarea'
+            />
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        </div>
+    )
 }
 
-export default App;
+export default App
